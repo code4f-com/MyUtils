ecad9c5009497d7cc4a1df2a1b32d8c89c187acf
tuanp <homlinhtinh@gmail.com@users.noreply.github.com>
2022-10-01 19:02:31 +0700

    26/9

--- src/main/java/com/tuanpla/utils/config/MyConstants.java
+++ src/main/java/com/tuanpla/utils/config/MyConstants.java
@@ -8,7 +8,7 @@ package com.tuanpla.utils.config;
  *
  * @author tuanp
  */
-public class MyConstants {
+public interface MyConstants {
 
     public static final String APPLICATION_JSON_UTF8 = "application/json;charset=UTF-8";
     public static final String APPLICATION_XML_UTF8 = "application/xml;charset=UTF-8";
