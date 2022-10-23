01cc685b3c65ba67d90243ffe39539a6ca565563
tuanp <homlinhtinh@gmail.com@users.noreply.github.com>
2022-10-23 21:34:57 +0700

    26/9

--- src/main/java/com/tuanpla/utils/logging/LogUtils.java
+++ src/main/java/com/tuanpla/utils/logging/LogUtils.java
@@ -24,7 +24,7 @@ public class LogUtils {
         String fullClassName = Thread.currentThread().getStackTrace()[2].getClassName();
         String className = fullClassName.substring(fullClassName.lastIndexOf(".") + 1);
         int lineNumber = Thread.currentThread().getStackTrace()[2].getLineNumber();
-        System.out.println(ConsoleColors.GREEN + PROJECT_NAME + ": " + ConsoleColors.BLUE + " INFO " + ConsoleColors.GREEN + className + ".class[d" + lineNumber + "] " + input);
+        System.out.println(ConsoleColors.GREEN + PROJECT_NAME + ": " + ConsoleColors.BLUE + " INFO " + ConsoleColors.GREEN + className + ".java [d" + lineNumber + "] " + input);
     }
 
     /**
@@ -37,7 +37,7 @@ public class LogUtils {
             String fullClassName = Thread.currentThread().getStackTrace()[2].getClassName();
             String className = fullClassName.substring(fullClassName.lastIndexOf(".") + 1);
             int lineNumber = Thread.currentThread().getStackTrace()[2].getLineNumber();
-            System.out.println(ConsoleColors.PURPLE + PROJECT_NAME + ":" + ConsoleColors.BLUE + " DEBUG " + ConsoleColors.PURPLE + className + ".class[d" + lineNumber + "] " + input);
+            System.out.println(ConsoleColors.PURPLE + PROJECT_NAME + ":" + ConsoleColors.BLUE + " DEBUG " + ConsoleColors.PURPLE + className + ".java [d" + lineNumber + "] " + input);
         }
     }
 
@@ -46,7 +46,7 @@ public class LogUtils {
             String fullClassName = Thread.currentThread().getStackTrace()[2].getClassName();
             String className = fullClassName.substring(fullClassName.lastIndexOf(".") + 1);
             int lineNumber = Thread.currentThread().getStackTrace()[2].getLineNumber();
-            System.err.println(ConsoleColors.RED + PROJECT_NAME + ": " + " ERROR " + ConsoleColors.RED + className + ".class[d" + lineNumber + "] " + input);
+            System.err.println(ConsoleColors.RED + PROJECT_NAME + ": " + " ERROR " + ConsoleColors.RED + className + ".java [d" + lineNumber + "] " + input);
         }
     }
 
