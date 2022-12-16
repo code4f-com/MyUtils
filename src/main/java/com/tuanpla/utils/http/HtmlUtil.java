759efe192d50b28dd8add8facb574e7e0368c6ed
Mac OS <7509414+tuanpla@users.noreply.github.com>
2022-12-16 16:50:47 +0700

    comit

--- src/main/java/com/tuanpla/utils/http/HtmlUtil.java
+++ src/main/java/com/tuanpla/utils/http/HtmlUtil.java
@@ -35,6 +35,7 @@ public class HtmlUtil {
         "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 1.1.4322; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET4.0C; .NET4.0E; InfoPath.3)",
         "Opera/9.80 (Windows NT 5.1; U; Edition Campaign 04; en) Presto/2.7.62 Version/11.01",
         "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.77 Safari/534.24",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:107.0) Gecko/20100101 Firefox/107.0",
         "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13",
         "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)",
         "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
